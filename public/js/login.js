059f85079cefa3ca54d111e9ae4e48256fb8d70e
VESSC-9591\Usuario <keisyangulo15@gmail.com>
2024-04-25 15:52:51 -0600

    Se agrega la opción para el front-end para que el usuario en el prompt coloque el codigo de verificación

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,24 @@
 
 VerifyUser = () => {
-
     const ajaxRequest = new XMLHttpRequest();
     ajaxRequest.addEventListener("load", function () {
-        // Handle the response here
+        
         if (ajaxRequest.status >= 200 && ajaxRequest.status < 300) {
             const data = JSON.parse(ajaxRequest.responseText);
             if (data.token) {
-                // Guardar el token en localStorage
                 localStorage.setItem('token', data.token);
                 console.log('Token guardado en localStorage');
-                window.location.href = 'profiles.html';
+                const verificationCode = data.verificationCode;
+                const userVerificationCode = prompt("Ingresa el código de verificación enviado a tu teléfono:");
+                if (userVerificationCode == verificationCode) {
+                    window.location.href = 'profiles.html';
+                } else {
+                    alert('Código de verificación incorrecto');
+                }
             } else {
+                alert('Error de autenticación');
                 console.error('Error de autenticación:', data.message);
             }
-
         } else {
            
         }
@@ -25,7 +29,6 @@ VerifyUser = () => {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
 
     let dataset = JSON.stringify({
         "email": email,
